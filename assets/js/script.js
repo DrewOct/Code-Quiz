cf0fe4d90c2f400ff481bea22679ad4f20db6a61
Andrew Seymour <andrew.seymour1@gmail.com>
2022-08-01 17:26:56 -0500

    displays correct or incorrect, timer intervals set

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,13 @@ var highScore = document.querySelector("#highScore");
 // return an array of questions
 var questions = document.querySelectorAll(".question");
 var qNumber = 0;
+var seconds = 75;
+var isGameRunning = false;
 
 quizPage.style.display = "none";
 highScore.style.display = "none";
+document.querySelector("#correct").style.display = "none";
+document.querySelector("#incorrect").style.display = "none";
 
 // nextQuestion execute the hide and reveal animations
 function nextQuestion() {
@@ -16,7 +20,9 @@ function nextQuestion() {
   var question = questions[qNumber];
   var answers = question.querySelectorAll(".answer");
   console.log(answers);
-  for (var i = 1; i < answers.length; i++) {
+  for (var i = 0; i < answers.length; i++) {
+    // add number to answer choices
+    answers[i].textContent = i + 1 + ". " + answers[i].textContent;
     if (answers[i].classList.contains("correct")) {
       answers[i].addEventListener("click", correctAnswer);
     } else if (answers[i].classList.contains("incorrect")) {
@@ -28,13 +34,14 @@ function nextQuestion() {
 // progression when answer incorrect
 function incorrectAnswer() {
   /// handle incorrect answer
-  console.log("incorrect");
   questions[qNumber].style.display = "none";
-
+  document.querySelector("#correct").style.display = "none";
+  document.querySelector("#incorrect").style.display = "block";
+  seconds -= 10;
   // increment qNumber by 1
   qNumber++;
   // if question is not last question, call nextQuestion
-  if (qNumber < questions.length) {
+  if (qNumber < questions.length && seconds > 0) {
     nextQuestion();
     // if last question, give player option to save score
   } else {
@@ -45,8 +52,9 @@ function incorrectAnswer() {
 // move player to next question when answer correct
 function correctAnswer() {
   ///handle correct answers
-  console.log("correct");
   questions[qNumber].style.display = "none";
+  document.querySelector("#correct").style.display = "block";
+  document.querySelector("#incorrect").style.display = "none";
 
   // increment qNumber by 1
   qNumber++;
@@ -60,6 +68,8 @@ function correctAnswer() {
 }
 
 function startQuiz() {
+  seconds = 75;
+  isGameRunning = true;
   quizPage.style.display = "block";
   for (var i = 1; i < questions.length; i++) {
     questions[i].style.display = "none";
@@ -69,10 +79,24 @@ function startQuiz() {
 }
 
 function saveScore() {
-    console.log("Save a score")
-    highScore.style.display = "block";
+  isGameRunning = false;
+  console.log("Save a score");
+  highScore.style.display = "block";
+  quizPage.style.display = "none";
 }
 startButton.addEventListener("click", startQuiz);
 // Create start card
 // EventHandler for button clicks
 // Pagination for cards
+
+function timer() {
+  if (isGameRunning) {
+    document.querySelector("#timer").textContent = seconds;
+    seconds--;
+    console.log(seconds);
+    if (seconds <= 0) {
+      saveScore();
+    }
+  }
+}
+setInterval(timer, 1000);
